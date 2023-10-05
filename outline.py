3fa6c42d4e7db1b28eef3de9c073beb3b2d23a4c
kiarachacon <143476708+kiarachacon@users.noreply.github.com>
2023-10-05 04:08:52 -0400

    General structure of the code

---
+++ outline.py
@@ -0,0 +1,19 @@
+# add imports necessary for the app
+# structure for pygame?
+
+# list out functions
+class adventure_game():
+    def text_generator():
+
+    def image_generator():
+
+    def animation_generator():
+
+    def story_collection():
+
+
+# Print out functions
+print(text_generator, image_generator, animation_generator, story_collection)
+
+# close out class
+# deploy app
